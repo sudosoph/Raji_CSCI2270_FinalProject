98e828b917272715b1bd60742a1a36f2d009eb6b
sudosoph <sophiaraji@gmail.com>
2015-04-29 02:39:42 -0600

    Added project files

---
+++ Genealogy/Genealogy.cpp
@@ -0,0 +1,9 @@
+//
+//  Genealogy.cpp
+//  Genealogy
+//
+//  Created by Sophie Gr on 4/29/15.
+//  Copyright (c) 2015 Sophia. All rights reserved.
+//
+
+#include "Genealogy.h"
---
+++ Genealogy/Genealogy.h
@@ -0,0 +1,14 @@
+//
+//  Genealogy.h
+//  Genealogy
+//
+//  Created by Sophie Gr on 4/29/15.
+//  Copyright (c) 2015 Sophia. All rights reserved.
+//
+
+#ifndef __Genealogy__Genealogy__
+#define __Genealogy__Genealogy__
+
+#include <stdio.h>
+
+#endif /* defined(__Genealogy__Genealogy__) */
